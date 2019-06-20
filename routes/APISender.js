0e5e3ba072c1e9d52481efe0cb640580201011e1
Savannah <savannahrayegonzales@gmail.com>
2019-06-20 12:22:05 -0500

    board sent

--- routes/APISender.js
+++ routes/APISender.js
@@ -2,14 +2,14 @@ var express = require("express");
 var router = express.Router();
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-//const Board = require('../models/board-model');
+const Board = require('../models/board-model');
 const User = require('../models/user-model');
 
 router.get("/", function(req, res, next) {
     // user = req.user;
     //Board = req.boards;
-    User.find(function(err, boards){
-        if(err) return console.error(err);
+    Board.find(function(err, boards){
+        if(err) return console.log("error nothing sent");
             console.log(boards);
         res.send(boards);
     });
