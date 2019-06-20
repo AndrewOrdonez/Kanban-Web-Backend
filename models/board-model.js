b0080e373976fced62c1ff17a703b031af8c9231
Savannah <savannahrayegonzales@gmail.com>
2019-06-20 12:08:58 -0500

    blarg

--- models/board-model.js
+++ models/board-model.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const User = require('./user-model');
+
 
 const boardSchema = new Schema({
     name: String,
     boardId: String,
     backgroundImg: String,
-    boardUsers: [userSchema],
+    boardUsers: [User],
     swimlanes: {
         title: String,
         id: String,
