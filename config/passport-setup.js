c88ff6e9f54b2bdf9d872eac109d0dac69ff5e5d
Savannah <savannahrayegonzales@gmail.com>
2019-06-21 10:17:18 -0500

    fix thumbnail

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -32,7 +32,8 @@ passport.use(
                 //if not, create user in our db
                 new User({
                     username: profile.displayName,
-                    googleId: profile.id
+                    googleId: profile.id,
+                    thumbnail: profile._json.picture
                 }).save().then((newUser) => {
                     console.log('new user created: ' + newUser);
                     done(null, newUser);
